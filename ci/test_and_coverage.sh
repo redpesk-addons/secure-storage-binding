5c5df22e913a3a4e482a5258f734b570a17d8300
Iheb Ben Gara Ali <iheb.bengaraali@iot.bzh>
2025-05-21 10:58:52 +0200

    Add coverage

--- ci/test_and_coverage.sh
+++ ci/test_and_coverage.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install or reinstall dependencies
-for p in afb-binding-devel afb-libpython afb-test-py afb-cmake-modules libdb-devel; do
+for p in afb-binding-devel afb-libpython afb-test-py afb-cmake-modules libdb-devel afb-helpers4; do
     sudo rpm -q $p && sudo dnf update -y $p || sudo dnf install -y $p 
 done
 
@@ -15,7 +15,7 @@ CMAKE_COVERAGE_OPTIONS="-DCMAKE_C_FLAGS=--coverage -DCMAKE_CXX_FLAGS=--coverage"
 rm -rf build
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=COVERAGE -DSECSTOREADMIN=ON ..
+cmake -DSECSTOREADMIN=ON ${CMAKE_COVERAGE_OPTIONS} ..
 make
 export AFB_PASSWD_DIR=$(pwd)
 mkdir -p ${AFB_PASSWD_DIR}
@@ -24,21 +24,23 @@ LD_LIBRARY_PATH=. python ../test/py-test.py -vvv
 
 
 
-# #
-# # Coverage
-# #
-# rm -f lcov_cobertura.py
-# wget https://raw.github.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
 
-# rm app.info
-# lcov --directory . --capture --output-file app.info
-# # remove system headers from coverage
-# lcov --remove app.info '/usr/*' -o app_filtered.info
-# # output summary (for Gitlab CI coverage summary)
-# lcov --list app_filtered.info
-# # generate a report (for source annotation in MR)
-# python lcov_cobertura.py app_filtered.info -o ./coverage.xml
+#
+# Coverage
+#
+cd ..
+rm -f lcov_cobertura*
+wget https://github.com/eriwen/lcov-to-cobertura-xml/releases/download/v2.1.1/lcov_cobertura-2.1.1.tar.gz
+tar xzvf lcov_cobertura*tar.gz
 
-# genhtml -o html app_filtered.info
+rm app.info
+lcov --directory . --capture --output-file app.info
+# remove system headers from coverage
+lcov --remove app.info '/usr/*' -o app_filtered.info
+# output summary (for Gitlab CI coverage summary)
+lcov --list app_filtered.info
+# generate a report (for source annotation in MR)
+PYTHONPATH=./lcov_cobertura-2.1.1/ python -m lcov_cobertura app_filtered.info -o ./coverage.xml
 
-# afb-client -H localhost:1234/api sec
+genhtml -o html app_filtered.info
+
